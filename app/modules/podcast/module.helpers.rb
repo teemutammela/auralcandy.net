ef1fd0de4ff4c192b2dfa85edadf162831882ef9
Teemu Tammela <teemu.tammela@auralcandy.net>
2020-07-07 10:46:11 +0300

    Decided that the track list scrambler was a stupid idea after all.

--- app/modules/podcast/module.helpers.rb
+++ app/modules/podcast/module.helpers.rb
@@ -44,42 +44,6 @@ module Sinatra
         "<![CDATA[#{string}]]>".html_safe
       end
 
-			# Scramble artist and track name if marker is found
-			def scramble_track_name(track)
-
-				# NOTE! This method was added as an countermeasure after we received a RIAA copyright claim.
-				# While we do not advocate for privacy, especially for profit, these copyright claims are utter BS.
-				# Granted, DJ mixes have always been bit of a gray area when it comes to copyright.
-				# Having said that, most artist and record labels view them as a positive thing and leave them be.
-				# In our opinion, going after non-profit podcasts is an overkill and counterproductive.
-				# We don't earn a single penny. Quite the contrary, we spend thousands doing free promotion for the music we love.
-
-				marker = "[SCRAMBLE]"
-
-				if track.include?(marker)
-					track		= track.gsub(marker, "").strip
-					artist	= scramble_string(track.split(" - ")[0])
-					track		= scramble_string(track.split(" - ")[1])
-					track		= [artist, track].join(" - ")
-				end
-
-				return track
-
-			end
-
-			# Scramble string by inserting random invisible characters
-			def scramble_string(str)
-
-				str = str.split("")
-
-				(0..(str.size/2).to_i).each do |i|
-					str.insert(rand(1..str.size), "<span style=\"display: none;\">#{[*"a".."z"].sample}</span>")
-				end
-
-				return str.join("")
-
-			end
-
     end
 
   end
