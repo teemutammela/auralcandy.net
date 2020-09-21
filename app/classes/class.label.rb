34269bdcd3fe8355e04be9956398912d755a906f
Teemu Tammela <teemu.tammela@auralcandy.net>
2020-09-21 09:25:15 +0300

    Renamed class 'Label' property 'link' --> 'link_url' for consistency.

--- app/classes/class.label.rb
+++ app/classes/class.label.rb
@@ -1,13 +1,13 @@
 class Label
 
   # Accessible properties
-  attr_accessor :name, :link
+  attr_accessor :name, :link_url
 
   # Initialize properties
   def initialize(entry)
 
-    @name = entry.fields[:name]
-    @link = entry.fields[:link_url]
+    @name			= entry.fields[:name]
+    @link_url	= entry.fields[:link_url]
 
   end
 
