c231845eef5794b40ac4a5c4fb485f76b74336bc
Teemu Tammela <teemu.tammela@kiskolabs.com>
2024-06-18 11:12:49 +0300

    Updated Ruby version and gems, refactored Episode's RFC date parsing.

--- app/classes/episode.rb
+++ app/classes/episode.rb
@@ -2,33 +2,29 @@
 
 class Episode
   # Accessible properties
-  attr_reader :id, :published, :updated, :dj, :title, :slug, :brand, :description, :track_list, :duration, :genre,
-              :labels, :image_url, :audio_url, :file_size, :downloads
+  attr_reader :id, :published, :published_rfc, :updated, :dj, :title, :slug, :brand, :description, :track_list,
+              :duration, :genre, :labels, :image_url, :audio_url, :file_size, :downloads
 
   # Initialize properties
   def initialize(entry)
-    @id           = entry.sys[:id]
-    @published    = Date.strptime(entry.fields[:release_date].tr('-', ''), '%Y%m%d')
-    @updated      = entry.sys[:updated_at]
-    @dj           = entry.fields[:dj].to_a.map { |dj| DJ.new(dj) }
-    @title        = entry.fields[:title]
-    @slug         = entry.fields[:slug]
-    @index        = $slugs.index(@slug).to_i
-    @brand        = Brand.new(entry.fields[:brand])
-    @description  = entry.fields[:description]
-    @track_list   = entry.fields[:track_list].to_s.split("\n")
-    @duration     = entry.fields[:duration]
-    @genre        = entry.fields[:genre].sort
-    @labels       = (entry.fields[:label].to_a.map { |label| Label.new(label) }).sort_by(&:name)
-    @image_url    = (entry.fields[:image] ? "https:#{entry.fields[:image].url}" : @brand.image_url)
-    @audio_url    = entry.fields[:file_url]
-    @file_size    = entry.fields[:file_size].to_i
-    @downloads    = entry.fields[:downloads].to_i.to_s
-  end
-
-  # Publish date in RFC2822 format
-  def published_rfc
-    @published.rfc2822
+    @id             = entry.sys[:id]
+    @published      = Date.strptime(entry.fields[:release_date].tr('-', ''), '%Y%m%d')
+    @published_rfc  = DateTime.parse(entry.fields[:release_date]).rfc2822
+    @updated        = entry.sys[:updated_at]
+    @dj             = entry.fields[:dj].to_a.map { |dj| DJ.new(dj) }
+    @title          = entry.fields[:title]
+    @slug           = entry.fields[:slug]
+    @index          = $slugs.index(@slug).to_i
+    @brand          = Brand.new(entry.fields[:brand])
+    @description    = entry.fields[:description]
+    @track_list     = entry.fields[:track_list].to_s.split("\n")
+    @duration       = entry.fields[:duration]
+    @genre          = entry.fields[:genre].sort
+    @labels         = (entry.fields[:label].to_a.map { |label| Label.new(label) }).sort_by(&:name)
+    @image_url      = (entry.fields[:image] ? "https:#{entry.fields[:image].url}" : @brand.image_url)
+    @audio_url      = entry.fields[:file_url]
+    @file_size      = entry.fields[:file_size].to_i
+    @downloads      = entry.fields[:downloads].to_i.to_s
   end
 
   # Publish date in natural language format
