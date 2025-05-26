66b168cc034f98992d3d1ce79765d789e3cf19a4
Teemu Tammela <teemu.tammela@kiskolabs.com>
2025-05-26 10:12:59 +0300

    Fixed episode list update event with history back and forward events.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,4 @@
 const Search = {
-
   /* Get current search data-parameters from episode list element */
   getSearchParameters: function () {
     const data = $('#episode-list').data()
@@ -7,10 +6,8 @@ const Search = {
   },
 
   /* Update episode list data-parameters */
-  // NOTE! Page value is updated by paging button events
   setSearchParameters: function (brand, genre, limit, order) {
     const list = $('#episode-list')
-
     list.data('brand', brand)
     list.data('genre', genre)
     list.data('limit', limit)
@@ -21,12 +18,12 @@ const Search = {
   setSearchState: function (data) {
     const search = $('#episode-search')
     const queryString = window.location.search
-    const brand = (data !== null ? data.brand : 'any')
-    const genre = (data !== null ? data.genre : 'any')
-    const limit = (data !== null ? parseInt(data.limit) : 15)
-    const order = (data !== null ? data.order : 'date-desc')
-    const id = (data !== null ? data.id : null)
-    let page = (data !== null ? data.page : 1)
+    const brand = data ? data.brand : 'any'
+    const genre = data ? data.genre : 'any'
+    const limit = data ? parseInt(data.limit) : 15
+    const order = data ? data.order : 'date-desc'
+    const id = data ? data.id : null
+    let page = data ? data.page : 1
 
     // Set search form selected values
     search.find('select#brand').val(brand)
@@ -34,43 +31,36 @@ const Search = {
     search.find('select#limit').val(limit)
     search.find('select#order').val(order)
 
-    // Handle history event's data content
-    if (data !== null) {
+    if (data) {
       const url = `/search/${brand}/${genre}/${limit}/${order}/${id}/${page}`
 
       // Update episode list data-parameters and content
-      Search.setSearchParameters(data.brand, data.genre, data.limit, data.order)
+      Search.setSearchParameters(brand, genre, limit, order)
+      $('#episode-list').data('page', page)
       Search.loadEpisodeList(url)
     } else {
-      // Get page value from URL-parameter if present
       if (queryString.length !== 0) {
         const urlParameters = new URLSearchParams(queryString)
-        page = parseInt(urlParameters.get('page'))
+        page = parseInt(urlParameters.get('page')) || 1
       }
 
-      // Update episode list page data-parameter
       $('#episode-list').data('page', page)
-
-      // Initialize episode search with default values
       Search.initSearchForm(false)
     }
   },
 
   /* Init page history event and add search parameters to URL */
   setUrlParameters: function () {
-    // Get values from episode list's data-parameters
     const data = $('#episode-list').data()
     const url = new URL(window.location)
 
-    // Set URL-parameters
     url.searchParams.set('brand', data.brand)
     url.searchParams.set('genre', data.genre)
     url.searchParams.set('limit', data.limit)
     url.searchParams.set('order', data.order)
     url.searchParams.set('page', data.page)
 
-    // Set history event
-    history.pushState(data, document.title, url)
+    history.pushState(null, '', url)
   },
 
   /* Load episode list and insert into target element */
@@ -79,22 +69,16 @@ const Search = {
     const loading = $('#loading-indicator')
     const interval = 333
 
-    // Fade out episode list
     list.fadeTo(interval, 0)
     loading.fadeTo(0, 1)
     loading.css('z-index', 99)
 
-    /* Insert response into element */
     list.load(url, function () {
-      // Fade in episode list
       list.fadeTo(interval, 1)
       loading.fadeTo(0, 0)
       loading.css('z-index', -1)
 
-      // Initialize paging buttons
       Search.initPagingButtons()
-
-      // Initialize Play-buttons in episode list
       Player.initPlayButtons()
     })
   },
@@ -105,17 +89,11 @@ const Search = {
       const list = $('#episode-list')
       const url = $(this).data('url')
 
-      // Update episode list page data-parameter
       list.data('page', $(this).data('page'))
-
-      // Set search parameters to URL
       Search.setUrlParameters()
-
-      // Scroll page to beginning of episode list
       document.getElementById('episode-list').scrollIntoView()
 
-      // Load requested page if target URL is present
-      if (typeof (url) !== 'undefined') {
+      if (typeof url !== 'undefined') {
         Search.loadEpisodeList(url)
       }
 
@@ -129,7 +107,6 @@ const Search = {
   initSearchForm: function (setUrlParameters) {
     const search = $('#episode-search')
 
-    // Update episode list data-parameters
     Search.setSearchParameters(
       search.find('select#brand').val(),
       search.find('select#genre').val(),
@@ -137,20 +114,16 @@ const Search = {
       search.find('select#order').val()
     )
 
-    // Initialize episode search by using data-parameters
     Search.getSearchParameters()
 
-    // Set search parameters to URL
     if (setUrlParameters === true) {
       Search.setUrlParameters()
     }
   }
-
 }
 
 /* Document ready state */
 $(document).ready(function () {
-  // Initialize play and paging buttons
   Player.initPlayButtons()
   Search.initPagingButtons()
 
@@ -159,12 +132,27 @@ $(document).ready(function () {
     Search.initSearchForm(true)
   })
 
-  window.addEventListener('popstate', function (event) {
-    const currentUrl = window.location.href.split('#')[0]
-    const previousUrl = event.state ? event.state.url.split('#')[0] : null
+  let lastUrlWithoutHash = window.location.origin + window.location.pathname + window.location.search
 
-    if (currentUrl !== previousUrl && !window.location.hash) {
-      Search.setSearchState(event.state)
+  // Update episode list when URL changes
+  window.addEventListener('popstate', function () {
+    const currentUrlWithoutHash = window.location.origin + window.location.pathname + window.location.search
+
+    if (currentUrlWithoutHash === lastUrlWithoutHash) {
+      return
     }
+
+    lastUrlWithoutHash = currentUrlWithoutHash
+
+    const urlParams = new URLSearchParams(window.location.search)
+    const data = {
+      brand: urlParams.get('brand') || 'any',
+      genre: urlParams.get('genre') || 'any',
+      limit: urlParams.get('limit') || 15,
+      order: urlParams.get('order') || 'date-desc',
+      page: urlParams.get('page') || 1
+    }
+
+    Search.setSearchState(data)
   })
 })
